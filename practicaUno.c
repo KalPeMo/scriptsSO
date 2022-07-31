4d917be22714696210427fc0a0aa59302392bdd8
KalPeMo <96583745+KalPeMo@users.noreply.github.com>
2022-07-30 21:49:35 -0500

    Update practicaUno.c

--- practicaUno.c
+++ practicaUno.c
@@ -137,8 +137,58 @@ void primerPunto(int fd){
 
 void segundoPunto(int fd){
 }
-void tercerPunto(int fd){
+
+int tercerPunto(int fd){
+    char filePath[100];
+    printf("\n3. Revisar si el archivo esta balanceado en apertura y clausura de signos de agrupacion (){}:");
+    printf("\n   Ingrese la ruta del archivo ");
+    scanf("%s", filePath);
+    printf("\n Ruta del archivo: %s\n", filePath);
+
+    FILE *filePointer;
+    char ch;
+    filePointer = fopen(filePath, "r");
+    if (filePointer == NULL){
+        printf("No se encontro archivo \n");
+        return -1;
+    } else {
+        read_all_messages_stack(fd);
+        while ((ch = fgetc(filePointer)) != EOF){
+            if(ch == '(' || ch == '{'){
+                char msgIn[2] = {ch, '\0'};
+                write_message(fd, BRIDGE_W_S, msgIn);
+            }
+            else if(ch == ')'){
+                if(send_empty_command(fd, BRIDGE_STATE_S) > 0){
+                    char msgOut[2];
+                    read_message(fd, BRIDGE_R_S, msgOut);
+                    if(msgOut[0] != '('){
+                        return 0;
+                    }
+                } else {
+                    return 0;
+                }
+            } else if(ch == '}'){
+                if(send_empty_command(fd, BRIDGE_STATE_S) > 0){
+                    char msgOut[2];
+                    read_message(fd, BRIDGE_R_S, msgOut);
+                    if(msgOut[0] != '{'){
+                        return 0;
+                    }
+                } else {
+                    return 0;
+                }
+            }
+        }
+        if(send_empty_command(fd, BRIDGE_STATE_S) > 0){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    fclose(filePointer);
 }
+
 void cuartoPunto(int fd){
 }
 void quintoPunto(int fd){
